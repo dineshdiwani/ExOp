c8d4a75e3ed40a9f3452b2d368ff5b359cb9b607
Dinesh Diwani <dineshdiwani@gmail.com>
2026-04-08 16:01:58 +0530

    Fix chat room runtime TDZ crash in initial hook deps

--- frontend/src/pages/ChatRoom.js
+++ frontend/src/pages/ChatRoom.js
@@ -117,7 +117,7 @@ export default function ChatRoom() {
         clearInterval(pollIntervalRef.current);
       }
     };
-  }, [fetchData, refreshRoomState]);
+  }, [bookingId]);
 
   useEffect(() => {
     scrollToBottom();
